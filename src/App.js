6f95f034ac5242e77fa849b5a2a6158999b321f1
Paulo Dionisio <devpaulodvo@gmail.com>
2022-02-06 01:24:11 +0800

    publishing

--- src/App.js
+++ src/App.js
@@ -11,12 +11,8 @@ import Projects from './Components/Projects/Projects';
 const App = () => {
   return(
     <React.Fragment>
-      <NavBar/>
-      <Switch>
-        <Route exact path='/' element={<Home/>}/>
-        <Route exact path='/portfolio' element={<Home/>}/>
-        <Route path='/projects' element={< Projects/>}/>
-      </Switch>
+      <Home/>
+      <Projects/>
     </React.Fragment>
   )
 }
